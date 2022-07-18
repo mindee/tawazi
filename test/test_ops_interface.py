067a19a222a5af566a90634eeffa3c991a7e53dd
bashir <bashir@mindee.co>
2022-07-18 16:33:25 +0200

    chg: :recycle: use a logger instead of prints during tests

--- test/test_ops_interface.py
+++ test/test_ops_interface.py
@@ -1,36 +1,38 @@
 from typing import Union
 from tawazi import op, to_dag
 import pytest
+from logging import Logger
 
+logger = Logger(name="mylogger", level="ERROR")
 
 def test_ops_interface():
     @op
     def a():
-        print('ran a')
+        logger.debug('ran a')
         return "a"
 
     @op
     def b(a):
-        print(f"a is {a}")
-        print('ran b')
+        logger.debug(f"a is {a}")
+        logger.debug('ran b')
         return "b"
 
     @op
     def c(a):
-        print(f"a is {a}")
-        print('ran c')
+        logger.debug(f"a is {a}")
+        logger.debug('ran c')
         return 'c'
 
     @op
     def d(b, c, third_argument: Union[str, int]=1234, fourth_argument=6789):
-        print(f"b is {b}")
-        print(f"c is {c}")
-        print(f'third argument is {third_argument}')
+        logger.debug(f"b is {b}")
+        logger.debug(f"c is {c}")
+        logger.debug(f'third argument is {third_argument}')
         pytest.third_argument = third_argument
-        print(f'fourth argument is {fourth_argument}')
+        logger.debug(f'fourth argument is {fourth_argument}')
         pytest.fourth_argument = fourth_argument
-        print(f"ran d")
-        # print(f"ran d {some_constant} {keyworded_arg}")
+        logger.debug(f"ran d")
+        # logger.debug(f"ran d {some_constant} {keyworded_arg}")
         return "d"
 
     @to_dag
@@ -42,17 +44,17 @@ def test_ops_interface():
 
     @op
     def e():
-        print("ran e")
+        logger.debug("ran e")
         return "e"
 
     @op
     def f(a, b, c, d, e):
-        print(f"a is {a}")
-        print(f"b is {b}")
-        print(f"c is {c}")
-        print(f"d is {d}")
-        print(f"e is {e}")
-        print("ran f")
+        logger.debug(f"a is {a}")
+        logger.debug(f"b is {b}")
+        logger.debug(f"c is {c}")
+        logger.debug(f"d is {d}")
+        logger.debug(f"e is {e}")
+        logger.debug("ran f")
 
     @to_dag
     def my_other_custom_dag():
@@ -64,22 +66,22 @@ def test_ops_interface():
         varf = f(vara, varb, varc, vard, vare)
 
     d1 = my_custom_dag()
-    print(f"\n1st execution of dag")
+    logger.debug(f"\n1st execution of dag")
     d1.execute()
     assert pytest.third_argument == 1234
     assert pytest.fourth_argument == 1111
-    print(f"\n2nd execution of dag")
+    logger.debug(f"\n2nd execution of dag")
     d1.execute()
 
     d2 = my_other_custom_dag()
-    print("\n1st execution of other dag")
+    logger.debug("\n1st execution of other dag")
     d2.execute()
     assert pytest.third_argument == 'blabla'
     assert pytest.fourth_argument == 2222
-    print("\n2nd execution of other dag")
+    logger.debug("\n2nd execution of other dag")
     d2.execute()
 
-    print(f"\n3rd execution of dag")
+    logger.debug(f"\n3rd execution of dag")
     d1.execute()
     assert pytest.third_argument == 1234
     assert pytest.fourth_argument == 1111
