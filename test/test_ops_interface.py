666519ee9a0fef63ea9b9591e4fd9141dd614f8a
Matthias Cremieux <matthias@mindee.co>
2022-08-16 15:04:59 +0200

    chg: :art: black and isort on all files with latest version

--- test/test_ops_interface.py
+++ test/test_ops_interface.py
@@ -1,35 +1,38 @@
-from typing import Union
-from tawazi import op, to_dag
-import pytest
 from logging import Logger
+from typing import Union
+
+import pytest
+
+from tawazi import op, to_dag
 
 logger = Logger(name="mylogger", level="ERROR")
+
 
 def test_ops_interface():
     @op
     def a():
-        logger.debug('ran a')
+        logger.debug("ran a")
         return "a"
 
     @op
     def b(a):
         logger.debug(f"a is {a}")
-        logger.debug('ran b')
+        logger.debug("ran b")
         return "b"
 
     @op
     def c(a):
         logger.debug(f"a is {a}")
-        logger.debug('ran c')
-        return 'c'
+        logger.debug("ran c")
+        return "c"
 
     @op
-    def d(b, c, third_argument: Union[str, int]=1234, fourth_argument=6789):
+    def d(b, c, third_argument: Union[str, int] = 1234, fourth_argument=6789):
         logger.debug(f"b is {b}")
         logger.debug(f"c is {c}")
-        logger.debug(f'third argument is {third_argument}')
+        logger.debug(f"third argument is {third_argument}")
         pytest.third_argument = third_argument
-        logger.debug(f'fourth argument is {fourth_argument}')
+        logger.debug(f"fourth argument is {fourth_argument}")
         pytest.fourth_argument = fourth_argument
         logger.debug(f"ran d")
         # logger.debug(f"ran d {some_constant} {keyworded_arg}")
@@ -61,7 +64,7 @@ def test_ops_interface():
         vara = a()
         varb = b(vara)
         varc = c(vara)
-        vard = d(varb, c=varc, fourth_argument=2222, third_argument='blabla')
+        vard = d(varb, c=varc, fourth_argument=2222, third_argument="blabla")
         vare = e()
         varf = f(vara, varb, varc, vard, vare)
 
@@ -76,7 +79,7 @@ def test_ops_interface():
     d2 = my_other_custom_dag()
     logger.debug("\n1st execution of other dag")
     d2.execute()
-    assert pytest.third_argument == 'blabla'
+    assert pytest.third_argument == "blabla"
     assert pytest.fourth_argument == 2222
     logger.debug("\n2nd execution of other dag")
     d2.execute()
