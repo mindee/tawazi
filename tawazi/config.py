b438321cdb71ee0717a2c00d38156d2fa3ea4b14
bashir <bashir@mindee.co>
2022-10-03 19:26:09 +0200

    chg: :refactor: :label: typing

--- tawazi/config.py
+++ tawazi/config.py
@@ -1,6 +1,6 @@
 import functools
 
-from .dag import ExecNode
+from tawazi import ExecNode
 
 
 def change_default_exec_node_sequential_strategy(is_sequential: bool) -> None:
@@ -9,4 +9,6 @@ def change_default_exec_node_sequential_strategy(is_sequential: bool) -> None:
     Changing the default strategy back and forth is not recommended
     """
     # TODO: Modifying the behaviour of the library like this might not be best practice
-    ExecNode.__init__ = functools.partialmethod(ExecNode.__init__, is_sequential=is_sequential)  # type: ignore
+    ExecNode.__init__ = functools.partialmethod(
+        ExecNode.__init__, is_sequential=is_sequential
+    )  # type: ignore
