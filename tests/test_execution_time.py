afbced85f59bc93606c7200643bba81988938dab
bashir <bashir@mindee.co>
2022-10-14 17:05:17 +0200

    test: :white_check_mark: check that the DAG is running in parallel

---
+++ tests/test_execution_time.py
@@ -0,0 +1,35 @@
+# type: ignore
+from time import sleep, time
+
+from tawazi import op, to_dag
+
+T = 0.1
+
+
+@op
+def a():
+    sleep(T)
+
+
+@op
+def b():
+    sleep(T)
+
+
+@op
+def c(a, b):
+    sleep(T)
+
+
+@to_dag(max_concurrency=2)
+def deps():
+    a_ = a()
+    b_ = b()
+    c_ = c(a_, b_)
+
+
+def test_timing():
+    t0 = time()
+    deps().execute()
+    execution_time = time() - t0
+    assert execution_time < 2.5 * T
