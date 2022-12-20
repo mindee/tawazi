55e0b8c3d42841bf1ba7dc969ed691b442580def
Matthias Cremieux <matthias@mindee.co>
2022-12-20 11:49:59 +0100

    chg: :recycle: use env vars by default instead of rewriting config manually in external use

--- tawazi/config.py
+++ tawazi/config.py
@@ -1,14 +1,14 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 
 
 class Config(BaseSettings):
-    TAWAZI_IS_SEQUENTIAL: bool = False
+    TAWAZI_IS_SEQUENTIAL: bool = Field(False, env="TAWAZI_IS_SEQUENTIAL")
 
     # Logger settings
-    LOGURU_LEVEL: str = "INFO"
-    LOGURU_BACKTRACE: bool = True
+    LOGURU_LEVEL: str = Field("CRITICAL", env="TAWAZI_LOGGER_LEVEL")
+    LOGURU_BACKTRACE: bool = Field(False, env="TAWAZI_LOGGER_BT")
     # Caution: to set to False if used in prod (exposes variable names)
-    LOGURU_DIAGNOSE: bool = False
+    LOGURU_DIAGNOSE: bool = Field(False, env="TAWAZI_LOGGER_DIAGNOSE")
 
 
 Cfg = Config()
