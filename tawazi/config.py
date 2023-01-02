0951d40fe15b955918db51975c751a96a66c0195
bashir <bashir@mindee.co>
2023-01-02 15:59:50 +0100

    fix: :bug: not a perfect solution

--- tawazi/config.py
+++ tawazi/config.py
@@ -5,10 +5,14 @@ class Config(BaseSettings):
     TAWAZI_IS_SEQUENTIAL: bool = False
 
     # Logger settings
-    LOGURU_LEVEL: str = "INFO"
+    LOGURU_LEVEL: str = "ERROR"
     LOGURU_BACKTRACE: bool = True
     # Caution: to set to False if used in prod (exposes variable names)
     LOGURU_DIAGNOSE: bool = False
 
 
 Cfg = Config()
+if Cfg.LOGURU_LEVEL == "PROD":
+    from loguru import logger
+
+    logger.disable("tawazi")
