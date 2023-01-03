4c44b48b52342c61b878d8a92322940c3c698c46
bashir <bashir@mindee.co>
2023-01-03 13:12:05 +0100

    chg: :white_check_mark: new tests

---
+++ tests/test_decorator.py
@@ -0,0 +1,40 @@
+# type: ignore
+from functools import wraps
+
+from tawazi import op, to_dag
+
+"""Integration test"""
+
+
+def my_little_logger(func):
+    @wraps(func)
+    def log(*args, **kwargs):
+        print("this should print before execution")
+        res = func(*args, **kwargs)
+        print("this should print after execution")
+        return res
+
+    return log
+
+
+@op
+@my_little_logger
+def a():
+    return "titi"
+
+
+@op
+@my_little_logger
+def b(a):
+    return "tata" + a
+
+
+@to_dag
+def pipe():
+    a_ = a()
+    t = b(a_)
+
+
+def test_decorator():
+    p = pipe()
+    p.execute()
