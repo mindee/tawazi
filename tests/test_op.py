4a98fe3419f914c1664b08d00d76938f5644130d
bashir <bashir@mindee.co>
2023-01-03 13:12:05 +0100

    chg: :white_check_mark: new tests

---
+++ tests/test_op.py
@@ -0,0 +1,67 @@
+# type: ignore
+from tawazi import op, to_dag
+
+"""integration test"""
+
+# tests different cases of @op decoration for Python functions
+# 1. different signatures
+@op
+def f1():
+    return 1
+
+
+@op
+def f2(f1):
+    return 2
+
+
+@op
+def f3(f1, f2):
+    assert f1 == 1
+    assert f2 == 2
+    return 3
+
+
+@op
+def f4(f1, cst=0):
+    # TODO: test with argument param and without argument param cst
+    return 4 + cst
+
+
+@op
+def f5(*args):
+    return sum(args)
+
+
+@op
+def f6(**kwargs):
+    return sum(kwargs.values())
+
+
+@op
+def f7(*args, **kwargs):
+    return sum(args) + sum(kwargs.values())
+
+
+@op
+def f8(f1, *args, **kwargs):
+    return sum([f1, *args, *(kwargs.values())])
+
+
+@to_dag
+def pipe():
+    _1 = f1()
+    # import ipdb
+    # ipdb.set_trace()
+    _2 = f2(_1)
+    _3 = f3(_1, _2)
+    _4 = f4(_1)
+    _5 = f5(_1, _2)
+    _6 = f6(foo=_1, bar=_2)
+    _7 = f7(_1, _2, foo=_1, bar=_2)
+    _8 = f8(_1, _2, _3)
+
+
+def test_ops_signatures():
+    p = pipe()
+    p.execute()
