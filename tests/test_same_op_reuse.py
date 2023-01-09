b1eeab96e9773b3a7c923dbe3131e9241cd0e379
bashir <bashir@mindee.co>
2023-01-09 15:53:20 +0100

    new: :sparkles: reuse the same operation multiple times

---
+++ tests/test_same_op_reuse.py
@@ -0,0 +1,21 @@
+# type: ignore
+from tawazi import op, to_dag
+
+
+@op
+def a(v):
+    return v + 1
+
+
+@to_dag
+def pipe():
+    a_1 = a(1)
+    a_2 = a(2)
+
+    return a_1, a_2
+
+
+def test_function_reuse():
+    a_1, a_2 = pipe()
+
+    assert a_1, a_2 == (2, 3)
