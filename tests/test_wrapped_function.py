2ee93e81322d05dc7183a1d46f1716b0968fbb19
bashir <bashir@mindee.co>
2023-01-11 08:46:13 +0100

    new: :sparkles: wrap the generated DAG as the pipeline description function when declared using @to_dag

---
+++ tests/test_wrapped_function.py
@@ -0,0 +1,37 @@
+# type: ignore
+from typing import List
+
+from tawazi import op, to_dag
+
+
+@op
+def abcd(i: int, b: List[str], cst: float = 0.1, **kwargs) -> int:
+    """doc of a"""
+    print(i, b, cst)
+    return i
+
+
+@to_dag
+def pipe(entry: int) -> int:
+    """doc of my pipeline"""
+    b = abcd(entry)
+    return b
+
+
+def test_doc_pipeline():
+    assert pipe.__doc__ == """doc of my pipeline"""
+
+
+def test_name_pipeline():
+    assert pipe.__name__ == "pipe"
+
+
+def test_doc_operation():
+    assert abcd.__doc__ == """doc of a"""
+
+
+def test_name_op():
+    assert abcd.__name__ == "abcd"
+
+
+# TODO: add assertion for type checking after doing some research!
