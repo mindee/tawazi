154602dd3862051bf2b41315218df2dc4155c8ec
bashir <bashir@mindee.co>
2023-01-25 12:04:43 +0100

    chg: :sparkles: intorudce NoValType and make tag a property

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -21,3 +21,18 @@ def test_tag():
     assert pipe() == 1236
     assert pipe.get_nodes_by_tag("b") == [pipe.get_node_by_id("a")]
     assert pipe.get_nodes_by_tag(("op", "b", "takes argument a")) == [pipe.get_node_by_id("b")]
+
+
+def test_call_tag():
+    @to_dag
+    def pipe():
+        a_ = a(10, twz_tag="another_a_tag")
+        b_ = b(a_, twz_tag="another_b_tag")
+        return b_
+
+    assert pipe() == 12
+    assert pipe.get_nodes_by_tag("another_a_tag") == [pipe.get_node_by_id("a")]
+    assert pipe.get_nodes_by_tag("another_b_tag") == [pipe.get_node_by_id("b")]
+
+
+test_call_tag()
