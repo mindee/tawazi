0f832821e50f1a4b96c1a3808b629bd64422aa83
bashir <bashir@mindee.co>
2023-01-26 16:36:58 +0100

    chg: :memo: document various parts in dag.py

--- tests/test_pipeline_input_output.py
+++ tests/test_pipeline_input_output.py
@@ -3,7 +3,7 @@
 import pytest
 
 from tawazi import op, to_dag
-from tawazi.errors import TawaziBaseException
+from tawazi.errors import TawaziArgumentException, TawaziBaseException
 
 
 @op
@@ -22,6 +22,11 @@ def declare_dag_function(input_img, cst: int = 0):
     return a(input_img, cst)
 
 
+@op
+def op1(in1):
+    return in1 + 1
+
+
 def test_pipeline_input_output():
     l = declare_dag_function([1, 2, 3], 10)
     assert l == 16
@@ -35,3 +40,21 @@ def test_pipeline_input_output_skipping_default_params():
 def test_pipeline_input_output_missing_argument():
     with pytest.raises(TawaziBaseException):
         l = declare_dag_function()
+
+
+def test_pipeline_default_args_input_not_provided():
+    @to_dag
+    def pipe(in1=1, in2=2, in3=3, in4=4):
+        return op1(in1), op1(in2), op1(in3), op1(in4)
+
+    assert pipe() == (2, 3, 4, 5)
+
+
+def test_pipeline_args_input_not_provided():
+    # should fail!!
+    @to_dag
+    def pipe(in1, in2, in3, in4):
+        return op1(in1), op1(in2), op1(in3), op1(in4)
+
+    with pytest.raises(TawaziArgumentException):
+        pipe()
