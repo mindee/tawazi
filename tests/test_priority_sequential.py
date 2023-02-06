30e883c944230a02cfc18ab7fa06647e752bba08
bashir ABDEL WAHED <bashir@minidee.co>
2023-02-06 15:15:22 +0100

    new: :sparkles: multiple return values (indexed return value of ExecNode)

--- tests/test_priority_sequential.py
+++ tests/test_priority_sequential.py
@@ -4,7 +4,7 @@ from typing import Any
 
 import pytest
 from tawazi import DAG, ErrorStrategy
-from tawazi.node import ExecNode
+from tawazi.node import ExecNode, UsageExecNode
 
 T = 0.01
 # global priority_sequential_comp_str
@@ -43,9 +43,9 @@ def test_priority() -> None:
     for _i in range(100):
         pytest.priority_sequential_comp_str = ""
         en_a = ExecNode(a.__name__, a, priority=1, is_sequential=False)
-        en_b = ExecNode(b.__name__, b, [en_a], priority=2, is_sequential=False)
-        en_c = ExecNode(c.__name__, c, [en_b], priority=2, is_sequential=False)
-        en_d = ExecNode(d.__name__, d, [en_a], priority=1, is_sequential=False)
+        en_b = ExecNode(b.__name__, b, [UsageExecNode(en_a)], priority=2, is_sequential=False)
+        en_c = ExecNode(c.__name__, c, [UsageExecNode(en_b)], priority=2, is_sequential=False)
+        en_d = ExecNode(d.__name__, d, [UsageExecNode(en_a)], priority=1, is_sequential=False)
         list_execnodes = [en_a, en_b, en_c, en_d]
 
         g: DAG[Any, Any] = DAG(list_execnodes, 1, behavior=ErrorStrategy.strict)
@@ -58,10 +58,10 @@ def test_sequentiality() -> None:
         # Sequentiality test
         pytest.priority_sequential_comp_str = ""
         en_a = ExecNode(a.__name__, a, is_sequential=False)
-        en_b = ExecNode(b.__name__, b, [en_a], priority=2, is_sequential=False)
-        en_c = ExecNode(c.__name__, c, [en_a], priority=2, is_sequential=False)
-        en_d = ExecNode(d.__name__, d, [en_b], priority=2, is_sequential=False)
-        en_e = ExecNode(e.__name__, e, [en_a], priority=1, is_sequential=True)
+        en_b = ExecNode(b.__name__, b, [UsageExecNode(en_a)], priority=2, is_sequential=False)
+        en_c = ExecNode(c.__name__, c, [UsageExecNode(en_a)], priority=2, is_sequential=False)
+        en_d = ExecNode(d.__name__, d, [UsageExecNode(en_b)], priority=2, is_sequential=False)
+        en_e = ExecNode(e.__name__, e, [UsageExecNode(en_a)], priority=1, is_sequential=True)
         list_execnodes = [en_a, en_b, en_c, en_d, en_e]
 
         g: DAG[Any, Any] = DAG(list_execnodes, 2, behavior=ErrorStrategy.strict)
