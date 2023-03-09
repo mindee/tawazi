120d3c90e7c21a1b61d5dd7de62c67fb5a98a94b
bashir ABDEL WAHED <bashir@mindee.co>
2023-03-09 16:11:07 +0100

    new: :white_check_mark: add tests for multiple return values for exec_nodes multiply indexed

--- tests/test_multiple_return_value_for_exec_node.py
+++ tests/test_multiple_return_value_for_exec_node.py
@@ -55,21 +55,44 @@ def test_lazy_exec_nodes_return_list_indexed() -> None:
     assert [2, 3, 4] == pipe()
 
 
-# def test_lazy_exec_nodes_return_multiple_index() -> None:
-#     @xn
-#     def generate_nested_list() -> List[int]:
-#         return [[1],2,[3,[4]]]
+def test_lazy_exec_nodes_return_multiple_index() -> None:
+    @xn
+    def generate_nested_list() -> Tuple[List[int], int, Tuple[int, Tuple[int]]]:
+        return ([1], 2, (3, (4,)))
 
-#     @xn
-#     def incr(a: int) -> int:
-#         return a + 1
+    @xn
+    def incr(a: int) -> int:
+        return a + 1
 
-#     @dag
-#     def pipe() -> List[int]:
-#         d = generate_nested_list()
-#         return [incr(d[0][0]), incr(d[1]), incr(d[2][0]), incr(d[2][1])]
+    @dag
+    def pipe() -> List[int]:
+        d = generate_nested_list()
+        return [incr(d[0][0]), incr(d[1]), incr(d[2][0]), incr(d[2][1][0])]
 
-#     assert [2, 3, 4] == pipe()
+    assert [2, 3, 4, 5] == pipe()
+
+
+def test_lazy_exec_nodes_return_multiple_index_reused() -> None:
+    @xn
+    def generate_nested_list() -> Tuple[List[int], int, Tuple[int, Tuple[int]]]:
+        return ([1], 2, (3, (4,)))
+
+    @xn
+    def incr(a: int) -> int:
+        return a + 1
+
+    @xn
+    def add(a: int, b: int) -> int:
+        return a + b
+
+    @dag
+    def pipe() -> Tuple[int, ...]:
+        d = generate_nested_list()
+        d0, d1, d2, d3 = d[0][0], d[1], d[2][0], d[2][1][0]
+        d0_, d1_, d2_, d3_ = incr(d0), incr(d1), incr(d2), incr(d3)
+        return add(d0_, d0), add(d1_, d1), add(d2_, d2), add(d3_, d3)
+
+    assert (3, 5, 7, 9) == pipe()
 
 
 def test_lazy_exec_nodes_multiple_return_values() -> None:
