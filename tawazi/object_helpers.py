6db9a8c4c13a2a73847e531cc1834931f95c370a
bashir ABDEL WAHED <bashir@mindee.co>
2023-03-24 16:01:24 +0100

    new: :sparkles: implement and_ or_ not_ as ExecNode for UsageExecNode

---
+++ tawazi/object_helpers.py
@@ -0,0 +1,26 @@
+"""Pre-instantiated objects that can be used as helpers."""
+
+from typing import Any, TypeVar, Union
+
+from .decorators import xn
+
+T = TypeVar("T")
+V = TypeVar("V")
+
+
+@xn
+def and_(a: T, b: V) -> Union[T, V]:
+    """Equivalent of `and` wrapped in ExecNode."""
+    return a and b
+
+
+@xn
+def or_(a: T, b: V) -> Union[T, V]:
+    """Equivalent of `or` wrapped in ExecNode."""
+    return a or b
+
+
+@xn
+def not_(a: Any) -> bool:
+    """Equivalent of `not` wrapped in ExecNode."""
+    return not a
