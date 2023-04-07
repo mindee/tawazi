701f5ad7cfb2ba2117efd20ac50d90674ce1f55f
bashir ABDEL WAHED <bashir@mindee.co>
2023-04-07 13:04:07 +0200

    new: :bookmark: bump versioon of tawazi to 0.3.0a0

--- tawazi/__init__.py
+++ tawazi/__init__.py
@@ -7,6 +7,6 @@ from .decorators import dag, xn
 from .errors import ErrorStrategy
 from .object_helpers import and_, not_, or_
 
-__version__ = "0.2.0"
+__version__ = "0.3.0a0"
 
 __all__ = ["DAG", "DAGExecution", "xn", "dag", "cfg", "ErrorStrategy", "and_", "or_", "not_"]
