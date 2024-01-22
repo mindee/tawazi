ad5b36a83845a61264016d3fe37d8af4ac4b6ab6
Matthias Cremieux <matthias@mindee.co>
2024-01-22 14:33:54 +0100

    chg: :recycle: :fire: remove stale tests, improve existing ones

--- tests/test_imbricated_dags.py
+++ tests/test_imbricated_dags.py
@@ -3,27 +3,31 @@ from typing import List
 from tawazi import dag, xn
 
 
+@xn
+def op1(img: List[int]) -> int:
+    return sum(img)
+
+
+@xn
+def op2(op1: int) -> int:
+    return 1 - op1
+
+
+@xn
+def op4(img: List[int]) -> int:
+    return sum(img) ** 2
+
+
+@xn
+def op5(mean: float, std: float) -> float:
+    return mean + std
+
+
 def test_imbricated_dags() -> None:
-    @xn
-    def op1(img: List[int]) -> int:
-        return sum(img)
-
-    @xn
-    def op2(op1: int) -> int:
-        return 1 - op1
-
     @xn
     @dag
     def op3(img: List[int]) -> int:
         return op2(op1(img))
-
-    @xn
-    def op4(img: List[int]) -> int:
-        return sum(img) ** 2
-
-    @xn
-    def op5(mean: float, std: float) -> float:
-        return mean + std
 
     @dag
     def op6(img: List[int]) -> float:
@@ -32,4 +36,4 @@ def test_imbricated_dags() -> None:
 
         return op5(titi, toto)
 
-    op6([1, 2, 3, 4])
+    assert op6([1, 2, 3, 4]) == 91
