3a59f44722b8c3eed591672062aefa51afd9cb76
bashir ABDEL WAHED <bashir@mindee.co>
2024-03-14 14:48:30 +0100

    fix: :bug: failing Tuple typing check with future annotations

---
+++ tests/test_future_typing.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from typing import Tuple, cast
+
+from tawazi import xn
+
+
+class A(int):
+    pass
+
+
+# can not be parametrized using pytest...
+def test_future_typing() -> None:
+    @xn(unpack_to=2)
+    def return_tuple() -> Tuple[int, int]:  # noqa: UP006
+        return 1, 2
+
+
+def test_future_typing_custom_class() -> None:
+    @xn(unpack_to=2)
+    def return_tuple() -> Tuple[A, A]:  # noqa: UP006
+        return cast(A, 1), cast(A, 2)
+
+
+def test_future_typing_int_ellipsis() -> None:
+    @xn(unpack_to=2)
+    def return_tuple() -> Tuple[int, ...]:  # noqa: UP006
+        return 1, 2, 3
