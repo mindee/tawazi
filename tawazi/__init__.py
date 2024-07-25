e9c30ab1b0ff36ee99b3d4b91a8a1ac6859e7ccd
bashir ABDEL WAHED <bashir@mindee.co>
2024-07-25 13:38:22 +0200

    chg: :recycle: expose AsyncDAGs as well

--- tawazi/__init__.py
+++ tawazi/__init__.py
@@ -1,7 +1,7 @@
 """tawazi is a package that allows parallel execution of a set of functions written in Python."""
 
 # exposing useful objects / Classes
-from ._dag import DAG, DAGExecution
+from ._dag import DAG, AsyncDAG, AsyncDAGExecution, DAGExecution
 from ._decorators import dag, xn
 from ._object_helpers import and_, not_, or_
 from .config import cfg
@@ -9,4 +9,16 @@ from .consts import Resource
 
 __version__ = "0.3.2"
 
-__all__ = ["DAG", "DAGExecution", "xn", "dag", "cfg", "and_", "or_", "not_", "Resource"]
+__all__ = [
+    "AsyncDAG",
+    "DAG",
+    "AsyncDAGExecution",
+    "DAGExecution",
+    "xn",
+    "dag",
+    "cfg",
+    "and_",
+    "or_",
+    "not_",
+    "Resource",
+]
