898046732053d5c386905ac0e09f5b64936bb41c
bashir ABDEL WAHED <bashir@mindee.co>
2024-08-02 17:20:56 +0200

    new: :white_check_mark: more tests + coverage

--- tests/test_error_location.py
+++ tests/test_error_location.py
@@ -31,15 +31,40 @@ def test_raise_error_location() -> None:
         dag_pipe()
 
 
+class NoneReturner:
+    def currentframe(self) -> None:
+        return None
+
+    @property
+    def f_back(self) -> None:
+        return None
+
+
 # if python's interpreter doesn't provide stack frame support, call_location won't work
 def test_raise_error_location_no_stack_frame_support(mocker: Any) -> None:
     # use mock to deactivate stack frame support
-    class NoneReturner:
-        def currentframe(self) -> None:
-            return None
 
     obj = NoneReturner()
     mocker.patch("tawazi.node.node.inspect", obj)
+
+    @xn
+    def faulty_function() -> int:
+        raise ValueError("This is a ValueError")
+
+    @dag
+    def pipe() -> int:
+        return faulty_function()
+
+    with pytest.raises(ValueError, match="This is a ValueError"):
+        pipe()
+
+
+def test_raise_error_location_no_stack_frame_support_shallow_call(mocker: Any) -> None:
+    class NoneReturnerShallow:
+        def currentframe(self) -> Any:
+            return NoneReturner()
+
+    mocker.patch("tawazi.node.node.inspect", NoneReturnerShallow())
 
     @xn
     def faulty_function() -> int:
