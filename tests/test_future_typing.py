3e8e351595b97721591cf8f3168403f5d9bc05c2
MaximeChurin <maxime@mindee.co>
2025-02-24 17:10:36 +0100

    fix

--- tests/test_future_typing.py
+++ tests/test_future_typing.py
@@ -1,6 +1,7 @@
+# ruff: noqa
 from __future__ import annotations
 
-from typing import cast
+from typing import Tuple, cast
 
 from tawazi import xn
 
@@ -12,17 +13,17 @@ class A(int):
 # can not be parametrized using pytest...
 def test_future_typing() -> None:
     @xn(unpack_to=2)
-    def return_tuple() -> tuple[int, int]:
+    def return_tuple() -> Tuple[int, int]:
         return 1, 2
 
 
 def test_future_typing_custom_class() -> None:
     @xn(unpack_to=2)
-    def return_tuple() -> tuple[A, A]:
+    def return_tuple() -> Tuple[A, A]:
         return cast(A, 1), cast(A, 2)
 
 
 def test_future_typing_int_ellipsis() -> None:
     @xn(unpack_to=2)
-    def return_tuple() -> tuple[int, ...]:
+    def return_tuple() -> Tuple[int, ...]:
         return 1, 2, 3
