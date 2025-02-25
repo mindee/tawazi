c16816148e222e00ca942fff04332569e60a5487
Bashir ABDEL WAHED <bashir@mindee.co>
2025-02-25 10:19:59 +0100

    chg: :bookmark: preparing v0.6.0

--- tests/test_unpack_legacy_typing.py
+++ tests/test_unpack_legacy_typing.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from typing import Tuple, cast
 
-from tawazi import xn
+from tawazi import xn, dag
 
 
 class A(int):
@@ -16,14 +16,32 @@ def test_future_typing() -> None:
     def return_tuple() -> Tuple[int, int]:
         return 1, 2
 
+    @dag
+    def my_dag() -> Tuple[int, int]:
+        return return_tuple()
+
+    assert my_dag() == (1, 2)
+
 
 def test_future_typing_custom_class() -> None:
     @xn(unpack_to=2)
     def return_tuple() -> Tuple[A, A]:
         return cast(A, 1), cast(A, 2)
 
+    @dag
+    def my_dag() -> Tuple[int, int]:
+        return return_tuple()
+
+    assert my_dag() == (1, 2)
+
 
 def test_future_typing_int_ellipsis() -> None:
     @xn(unpack_to=2)
     def return_tuple() -> Tuple[int, ...]:
         return 1, 2, 3
+
+    @dag
+    def my_dag() -> Tuple[int, ...]:
+        return return_tuple()
+
+    assert my_dag() == (1, 2)
